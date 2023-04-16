ea885a5af4dd3d6dec44cc1b512078b41e395469
MatthewChang908 <115748886+MatthewChang908@users.noreply.github.com>
2023-04-16 11:01:43 -0500

    updated tripcard

--- components/TripCard.js
+++ components/TripCard.js
@@ -1,17 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {doc, getDoc, updateDoc, collection, query, where, getDocs} from "firebase/firestore"
+import { auth, db } from '../firebase'
+import { useNavigation } from '@react-navigation/core'
 const TripCard = (props) => {
     const date = new Date(parseFloat(props.time) * 1000); // multiply by 1000 to convert seconds to milliseconds
     const month = date.getMonth() + 1; // add 1 to convert zero-based index to one-based index
     const day = date.getDate();
     const time = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-  
+    const [show, setShow] = useState(false)
+    const navigation = useNavigation();
+    const handlePress = () => {
+        setShow(!show)
+    }
+
+    const handleJoinGroup = () => {
+        storeData();
+        navigation.navigate("Home")
+    }
+
+    const storeData = async () => {
+        const docRef = doc(db, "Users", auth.currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            const user = docSnap.data();
+            const newTrips = user.trips;
+            console.log("newTrips: ", newTrips);
+            newTrips.push(props.id);
+            await updateDoc(docRef, {
+                trips: newTrips
+            });
+        }
+
+        // const tripRef = doc(db, "Trips", props.id);
+        // const tripSnap = await getDoc(tripRef);
+        // if (tripSnap.exists()) {
+        //     const trip = tripSnap.data();
+        //     const newRiders = trip.users;
+        //     const curNum = trip.numOfUsers;
+        //     newRiders.push(auth.currentUser.uid);
+        //     await updateDoc(tripRef, {
+        //         users: newRiders,
+        //         numOfUsers: curNum + 1,
+        //     });
+        // }
+    }
+
+
   return (
-    <View>
-      <Text>Destination: {props.destination}</Text>
-      <Text>Pickup Location: {props.pickup}</Text>
-      <Text>{month}/{day} at {time}</Text>
+    <View className='border'>
+        <TouchableOpacity onPress={() => handlePress()}>
+            <Text>Destination: {props.destination}</Text>
+            <Text>Pickup Location: {props.pickup}</Text>
+            <Text>{month}/{day} at {time}</Text>
+        </TouchableOpacity>
+        {show && (
+            <TouchableOpacity
+            onPress={() => handleJoinGroup()}
+            >
+                <Text>Join Trip</Text>
+            </TouchableOpacity>
+        )}
+      
     </View>
   )
 }
